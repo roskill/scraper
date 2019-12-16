e8f87e5dbe7f585ea1103aff14a2ba052c14bc17
Justin Liu <justin.liu@myob.com>
2019-12-16 15:30:54 +1100

    Log scraped data

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 
 const getData = async () => {
-  const response = await fetch(
+  const result = await axios.get(
     'https://www.nationalregisterofbigtrees.com.au/tree_register.php?lstatus_id=1'
   );
-  const data = await response.json();
-  console.log(data);
+  return result.data;
 };
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/', async (req, res) => {
+  const data = await getData();
+  console.log(data);
+});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
